e8b03336b6ba8254420e336007fe5eada7e4c995
Josh Rojas <rojos@seas.upenn.edu>
2014-02-13 00:14:38 -0500

    First commit for new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoviesWithFriends::Application.config.secret_key_base = '924d56ba54d39a636a29b0bfe8a371b535cbe25f543b90454bb5cbaa17338377a5ad42c34d0a4c29789e33f611b5bf4395a270034b1dd6977c1415ac86cd8a05'
