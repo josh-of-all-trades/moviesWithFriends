8f82911c05dcc939e2f10feaf9508f93a400a091
Josh Rojas <rojos@seas.upenn.edu>
2014-02-17 12:18:11 -0500

    editing controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 MoviesWithFriends::Application.routes.draw do
   
+  get "movies/new"
+  get "movies/index"
+  get "movies/show"
+  get "movies/edit"
+  get "events/new"
+  get "events/index"
+  get "events/show"
+  get "events/edit"
+  get "users/new"
+  get "users/index"
+  get "users/show"
+  get "users/edit"
   get "home/index"
   resources :users
   resources :events
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -11,4 +11,14 @@ class EventsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
 end
