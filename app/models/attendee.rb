5c995f0e759757eae2c2a70f1a37470fa1020037
Josh Rojas <rojos@seas.upenn.edu>
2014-04-07 12:33:33 -0400

    fixed problem with queries and limited attendence

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,5 +2,6 @@ class Attendee < ActiveRecord::Base
   belongs_to :attendee, :class_name => "User"
   belongs_to :event_attending, :class_name => "Event"
 
-  validates :attendee_id, uniqueness: {scope: :attendee_id, message: "you are already going to this event"}
+  validates :attendee_id, uniqueness: {scope: :event_attending_id, message: "you are already going to this event"}
+
 end
