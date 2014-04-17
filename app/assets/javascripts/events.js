521c1f54efecc89c64648ac48e3ff3b947bed54b
Josh Rojas <rojos@seas.upenn.edu>
2014-04-16 23:40:11 -0400

    jquery/javascript homework 10

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".btn").click(function(){
+		if ($(".attend").hasClass("hide")){
+			$(".attend").removeClass("hide");
+			$(".btn").text("Show Attendees");
+		} else {
+			$(".attend").addClass("hide");
+			$(".btn").text("Hide Attendees");
+		}
+	});
+});
